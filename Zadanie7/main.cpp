bb5953e4ee4f07579838798e4452d7672ac74957
rafalbalcerzak <raffi.balcer@gmail.com>
2019-11-13 14:46:40 +0100

    wskazniki

--- Zadanie7/main.cpp
+++ Zadanie7/main.cpp
@@ -1,39 +1,36 @@
 #include <iostream>
-#include "complex.h"
 using namespace std;
 
-//główna pętla tak
+void zamien(int *arg1, int *arg2){
+	int temp = *arg1;
+	*arg1 = *arg2;
+	*arg2 = temp; 
+}
+
 int main(){
-	Complex c1(2,3);
-	Complex c2(4,7);
-	Complex c3(0,0);
-	Complex c4(4);
-	Complex c5;
+	int a = 5;
+	int b = 7;
 
-	cout<<"konstruktor z 2 argumentami:"<<endl;
-	c1.show();
+	zamien(&a,&b);
+	cout<<"a: "<<a<<endl;
+	cout<<"b: "<<b<<endl;
 
-	cout<<"konstruktor z 1 argumentem:"<<endl;
-	c4.show();
-
-	cout<<"konstruktor bez argumentow:"<<endl;
-	c5.show();
-
-	cout<<"Dodawanie:"<<endl;
-	c3 = c1 + c2;
-	c3.show();
-
-	cout<<"Odejmowanie:"<<endl;
-	c3 = c1 - c2;
-	c3.show();
-
-	cout<<"Mnozenie:"<<endl;
-	c3 = c1 * c2;
-	c3.show();
-
-	cout<<"Modul z c1:"<<endl;
-	cout<<c1.mod()<<endl;
-
-	//cout<<"wynik:"<<c3<<endl; test
 	return 0;
 }
+
+
+
+// int b1;
+// double b2;
+
+// int *p1;
+// double *p2;
+
+// p1 = &b1;
+// p2 = &b2;
+
+// *p1 = 11;
+// *p2 = 1.5;
+
+// int **pp= &p1; //wskażnik do wskażnika
+
